63f2314cd0fcc628d7646dd03c975af092f7d0e4
Insu Park <facessu@gmail.com>
2020-01-14 14:24:34 +0900

    create UIView in ProductsC, because products header will be deleted

--- BirdViewChallenge/Controllers/ProductsController.swift
+++ BirdViewChallenge/Controllers/ProductsController.swift
@@ -20,6 +20,9 @@ class ProductsController: ViewController {
     let productsService: ProductsServiceType = ProductsService()
 //    var page: Int = 2
     
+    // PickerView Property
+    let temporaryArray = ["모든 피부 타입", "지성", "건성", "민감성"]
+    
     // MARK: - UI Properties
     
     lazy var collectionView: UICollectionView = {
@@ -31,7 +34,7 @@ class ProductsController: ViewController {
         cv.setCollectionViewLayout(layout, animated: true)
         cv.dataSource = self
         cv.delegate = self
-        cv.register(ProductsHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: ProductsHeader.self))
+//        cv.register(ProductsHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: ProductsHeader.self))
         cv.register(ProductsFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: String(describing: ProductsFooter.self))
         cv.register(ProductsCell.self, forCellWithReuseIdentifier: String(describing: ProductsCell.self))
         
@@ -47,6 +50,51 @@ class ProductsController: ViewController {
         sb.delegate = self
         return sb
     }()
+    
+    let headerView: UIView = {
+        let view = UIView()
+        let bottomBorder = UIView()
+        view.backgroundColor = .white
+        view.addSubview(bottomBorder)
+        bottomBorder.backgroundColor = UIColor.rgb(r: 33, g: 47, b: 62, a: 0.1)
+        bottomBorder.snp.makeConstraints { (m) in
+            m.leading.equalToSuperview()
+            m.trailing.equalToSuperview()
+            m.bottom.equalToSuperview()
+            m.height.equalTo(1)
+        }
+        return view
+    }()
+    
+    let typeName: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.text = "모든 피부 타입"
+        label.textColor = UIColor.colorWithHexString(hexString: Tools.color.black)
+        label.font = Tools.font.appleSDGothicNeoBold(size: 14)
+        return label
+    }()
+
+    lazy var typeButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "arrow_down_black"), for: .normal)
+        btn.addTarget(self, action: #selector(actionClickButton), for: .touchUpInside)
+        return btn
+    }()
+
+    @objc func actionClickButton() {
+//        delegate?.actionButton()
+        print("???")
+    }
+    
+    lazy var pickerView: UIPickerView = {
+        let pv = UIPickerView()
+        pv.backgroundColor = .black
+//        pv.delegate = self
+        pv.isHidden = false
+        return pv
+    }()
+    
     
     // MARK:- Initialize
     
@@ -72,16 +120,54 @@ class ProductsController: ViewController {
     
     override func setupUIComponents() {
         
-        // UICollectionView
+        // Components in VC
         
-        [self.collectionView].forEach { self.view.addSubview($0) }
-        collectionView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        [headerView, collectionView].forEach { self.view.addSubview($0) }
+        
+        let height = Int((UIApplication.shared.statusBarFrame.height) ?? 0 ) + Int((self.navigationController?.navigationBar.frame.height) ?? 0.0)
+        headerView.snp.makeConstraints { (m) in
+            m.top.equalToSuperview().offset(height)
+            m.leading.equalToSuperview()
+            m.trailing.equalToSuperview()
+            m.height.equalTo(50)
+        }
+        collectionView.snp.makeConstraints { (m) in
+            m.top.equalTo(headerView.snp.bottom)
+            m.leading.equalToSuperview()
+            m.trailing.equalToSuperview()
+            m.bottom.equalToSuperview()
+        }
+        
+        // Components in Header View
+        
+        [typeName, typeButton].forEach { self.headerView.addSubview($0) }
+
+        typeButton.snp.makeConstraints { (m) in
+            m.centerY.equalToSuperview()
+            m.trailing.equalToSuperview().offset(-12)
+            m.width.equalTo(24)
+            m.height.equalTo(24)
+        }
+
+        typeName.snp.makeConstraints { (m) in
+            m.centerY.equalTo(typeButton.snp.centerY)
+            m.trailing.equalTo(typeButton.snp.leading).offset(5)
+        }
         
         
         // UISearchBar
         
         navigationController?.navigationBar.addSubview(searchBar)
         
+        // UIPickerView
+        
+//        [pickerView].forEach { self.view.addSubview($0) }
+//        pickerView.snp.makeConstraints { (m) in
+//            m.leading.equalToSuperview()
+//            m.trailing.equalToSuperview()
+//            m.bottom.equalToSuperview()
+//            m.height.equalTo(240)
+//        }
         
         // - Access To UISearchBar Glass Icon
         
@@ -150,10 +236,10 @@ extension ProductsController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
-        case UICollectionView.elementKindSectionHeader:
-            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: String(describing: ProductsHeader.self), for: indexPath) as! ProductsHeader
-            header.backgroundColor = .white
-            return header
+//        case UICollectionView.elementKindSectionHeader:
+//            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: String(describing: ProductsHeader.self), for: indexPath) as! ProductsHeader
+//            header.backgroundColor = .white
+//            return header
         case UICollectionView.elementKindSectionFooter:
             let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: String(describing: ProductsFooter.self), for: indexPath) as! ProductsFooter
             footer.backgroundColor = .blue
@@ -181,9 +267,9 @@ extension ProductsController: UICollectionViewDelegateFlowLayout {
     
     
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: self.view.frame.width, height: 50)
-    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//        return CGSize(width: self.view.frame.width, height: 50)
+//    }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: self.view.frame.width, height: 50)
@@ -232,3 +318,36 @@ extension ProductsController: UICollectionViewDelegateFlowLayout {
     
     
 }
+
+//extension ProductsController: ProductsHeaderDelegate {
+//
+//    func actionButton() {
+//        self.pickerView.isHidden = true
+//        print("sss")
+//    }
+//
+//}
+
+// MARK:- UIPickerView
+//
+//extension ProductsController: UIPickerViewDelegate, UIPickerViewDataSource {
+//
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return self.temporaryArray.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return self.temporaryArray[row]
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//
+//    }
+//
+//
+//}
--- BirdViewChallenge/View/ProductsHeader.swift
+++ BirdViewChallenge/View/ProductsHeader.swift
@@ -8,109 +8,40 @@
 
 import UIKit
 
+//protocol ProductsHeaderDelegate: class {
+//    func actionButton()
+//}
+
 class ProductsHeader: UICollectionReusableView {
     
-    // MARK:- UI Properties
+//    // MARK:- Properties
+//
+//    weak var delegate: ProductsHeaderDelegate?
+//
+//    // MARK:- UI Properties
+//
+
+//
+
+//
+//    // MARK:- Initialize
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setupUIComponents()
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    // MARK:- UI Method
+//
+//    func setupUIComponents() {
+
+//
+//    }
     
-    let typeName: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .right
-        label.text = "모든 피부 타입"
-        label.textColor = UIColor.colorWithHexString(hexString: Tools.color.black)
-        label.font = Tools.font.appleSDGothicNeoBold(size: 14)
-        return label
-    }()
-    
-    let typeButton: UIButton = {
-        let btn = UIButton()
-        btn.setImage(UIImage(named: "arrow_down_black"), for: .normal)
-        btn.addTarget(self, action: #selector(actionChooseType), for: .touchUpInside)
-        return btn
-    }()
-    
-    @objc func actionChooseType() {
-//        self.pickerView.isHidden = false
-    }
-    
-    lazy var pickerView: UIPickerView = {
-        let pv = UIPickerView()
-        pv.backgroundColor = .white
-        pv.delegate = self
-//        pv.isHidden = true
-        return pv
-    }()
-    
-    let bottomBorder: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.rgb(r: 33, g: 47, b: 62, a: 0.1)
-        return view
-    }()
-    
-    // MARK:- Initialize
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        //        backgroundColor = .white
-        //        self.setupUIComponents()
-        //        self.imageView.rotate()
-        setupUIComponents()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK:- Methods
-    
-    func setupUIComponents() {
-        //        backgroundColor = .blue
-        
-        [typeName, typeButton, bottomBorder, pickerView].forEach { self.addSubview($0) }
-        
-        typeButton.snp.makeConstraints { (m) in
-            m.centerY.equalToSuperview()
-            m.trailing.equalToSuperview().offset(-12)
-            m.width.equalTo(24)
-            m.height.equalTo(24)
-        }
-        
-        typeName.snp.makeConstraints { (m) in
-            m.centerY.equalTo(typeButton.snp.centerY)
-            m.trailing.equalTo(typeButton.snp.leading).offset(5)
-        }
-        
-        bottomBorder.snp.makeConstraints { (m) in
-            m.leading.equalToSuperview()
-            m.trailing.equalToSuperview()
-            m.bottom.equalToSuperview()
-            m.height.equalTo(1)
-        }
-        
-    }
-    
-    let temporaryArray = ["모든 피부 타입", "지성", "건성", "민감성"]
 }
 
-// MARK:- UIPickerView
 
-extension ProductsHeader: UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return self.temporaryArray.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return self.temporaryArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        // 타입을 선택했을 때 처리해줄 메서드 정의
-    }
-    
-    
-}
