966f474eb3f70e41d600667e20bf05bad5fddcab
Insu Park <facessu@gmail.com>
2020-01-18 16:51:22 +0900

    ProductImageCell is made

--- BirdViewChallenge/View/Product/ProductImageCell.swift
+++ BirdViewChallenge/View/Product/ProductImageCell.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2020 inswag. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProductImageCell: UITableViewCell {
+    
+    // MARK:- UI Properties
+    
+    let productImage: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .red
+        return iv
+    }()
+    
+    // MARK:- Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: String(describing: ProductController.self))
+        self.setupUIComponents()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupUIComponents() {
+        backgroundColor = .red
+//        selectionStyle = .none
+        layer.cornerRadius = 30
+        clipsToBounds = true
+        layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        [productImage].forEach { self.addSubview($0) }
+        productImage.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
